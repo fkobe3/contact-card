be0d9edd508493dc5c975082c31a0307ce47cd5c
Frank Kobe <fkobe3@gmail.com>
2022-10-13 02:11:53 -0400

    indexedDB API

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -6,6 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+import { initdb } from './database';
+
+window.addEventListener('load', function() {
+  initdb();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
 
 window.addEventListener('load', function () {
     document.getElementById('logo').src = Logo;
